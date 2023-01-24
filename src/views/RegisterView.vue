8a52d6bc204335c49e8f1ac884f55610a398104b
WooCommander <ocnelavoc@gmail.com>
2023-01-24 07:18:34 +0200

    step2

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="Register">
-    <h1>Register</h1>
 
+    <div class="container">
+      <h1>Register</h1>
+      {{ registerModel }}
+      <div class="field">
+        <input type="email" placeholder="email" v-model="registerModel.email" />
+      </div>
+      <div class="field">
+        <input type="password" placeholder="password" v-model="registerModel.password" />
+      </div>
+      <button @click="onRegister">Register</button>
+    </div>
   </div>
 </template>
 
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+import AuthhenticationService from '@/services/AuthenticationService'
+defineProps<{
+  msg: string
+}>()
+const registerModel: { email: string, password: string } = reactive({ email: "", password: "" })
+const onRegister = async () => {
+  const response = await AuthhenticationService.register({ email: registerModel.email, password: registerModel.password });
+  console.log(response.data);
+
+}
+</script>
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .Register {
     min-height: 100vh;
     display: flex;
     align-items: center;
