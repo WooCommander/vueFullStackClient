b358eadf4662ac5d5e9e05e4152ea3735315f732
ocnelavoc <ocnelavoc@gmail.com>
2023-02-07 10:28:03 +0200

    добавил less sass и вывод ошибки

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -9,7 +9,7 @@
       <div class="field">
         <input type="password" placeholder="password" v-model="registerModel.password" />
       </div>
-      <div v-if="Error" class="error">{{ Error }}</div>
+      <div v-if="Error.message" class="error">{{ Error.message }}</div>
       <button @click="onRegister">Register</button>
       <div>Response:{{ result }}</div>
     </div>
@@ -26,14 +26,15 @@ defineProps<{
 }>()
 const registerModel: { email: string, password: string } = reactive({ email: "", password: "" })
 const result: { message: string } = reactive({ message: "" });
-let Error: any = reactive();
+let Error: any = reactive({ message: "" });
 const onRegister = async () => {
   try {
     const response = await AuthhenticationService.registerPost({ email: registerModel.email, password: registerModel.password });
     result.message = response.data.message;
     console.log(response.data);
+    Error.message = ""
   } catch (error: any) {
-    Error = error.response.data.error;
+    Error.message = error.response.data.error;
   }
 
 
@@ -41,9 +42,11 @@ const onRegister = async () => {
 }
 
 </script>
-<style>
-.Register .error {
-  color: red;
+<style lang="less">
+.Register {
+  .error {
+    color: red;
+  }
 }
 
 
